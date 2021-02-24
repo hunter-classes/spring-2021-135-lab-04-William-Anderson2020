10a8f93c0db5e1b3a571da96a8e81e4a580c35ad
William Anderson <48596907+William-Anderson2020@users.noreply.github.com>
2021-02-24 17:48:04 -0500

    start task a

---
+++ box.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+string draw_box(int h, int w){
+    string shape = "";
+    for (int i = 0; i < h; i++){
+        string line = "";
+        for (int j = 0; i < j; i++){
+            line = line + "*";
+        };
+        shape = shape + "\n";
+    };
+    return shape;
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+#include "box.h"
+
+int main(){
+    draw_box(4,4);
+    return 0;
+}
