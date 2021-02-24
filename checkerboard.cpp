66fa71af6f94aaa9b62f749a37f32aee5192dc29
William Anderson <48596907+William-Anderson2020@users.noreply.github.com>
2021-02-24 18:43:25 -0500

    checkerboard init test

---
+++ checkerboard.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string draw_box(int width, int height){
+    string shape;
+    string fill_char = "*";
+    for (int i = 0; i < height; i++){
+        string line = "";
+        if (i % 2 == 0){
+                line += "*";
+            }else{
+                line += " ";
+            };
+        for (int j = 0; j < width; j++){
+            if (line.substr(line.length() - 1) == "*"){
+                line += " ";
+            }else{
+               line += "*"; 
+            };
+        };
+        line += "\n";
+        shape += line;
+    };
+    return shape;
+};
--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 #include "box.h"
+#include "checkerboard.h"
 
 int main(){
-    cout << draw_box(4,4) << "\n";
-    cout << "TEST\n";
+    cout << draw_box(4,4);
+    cout << draw_checkerboard(7,7);
     return 0;
 }
