be84ef7c54821ac6d1ebfe57edf3c0affba65d51
William Anderson <48596907+William-Anderson2020@users.noreply.github.com>
2021-02-24 18:10:15 -0500

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -6,7 +6,7 @@ string draw_box(int width, int height){
     string shape;
     for (int i = 0; i < height; i++){
         string line = "";
-        for (int j = 0; i < width; j++){
+        for (int j = 0; j < width; j++){
             line += "*";
         };
         line += "\n";
