6977aa7472068c2f88ef4834e52c0a5f15fb2a02
William Anderson <48596907+William-Anderson2020@users.noreply.github.com>
2021-02-24 20:16:35 -0500

    Update cross.cpp

--- cross.cpp
+++ cross.cpp
@@ -38,12 +38,11 @@ string draw_cross(int size){
             };
             cout << line << " " << line_num << endl;
             line += "\n";
-            if(mid_line_marker == true && line_num == size - size/2){
+            if(mid_line_marker == true && line_num == size - (size/2 + 1)){
                 cout << "FIRE\n";
             };
             shape += line;    
         };
-        cout << size - size/2;
     };
     return shape;
 };
