babcae677f388d0b28eeec971c9dea4acda8fb9f
William Anderson <48596907+William-Anderson2020@users.noreply.github.com>
2021-02-24 22:23:44 -0500

    Update checkerboard3x3.cpp

--- checkerboard3x3.cpp
+++ checkerboard3x3.cpp
@@ -9,7 +9,7 @@ string draw_checkerboard3x3(int width, int height){
     for(int i = 0; i < height; i++){
         if(i % 3 == 0 && i > 0){
             if(height_first_char == "*"){
-                height_first_char = " ";
+                height_first_char = "a";
             }else{
                 height_first_char = "*";
             };
@@ -20,7 +20,7 @@ string draw_checkerboard3x3(int width, int height){
         for(int j = 0; j < width; j++){
             if(j % 3 == 0 && j > 0){
                 if(width_char == "*"){
-                    width_char = " ";
+                    width_char = "a";
                 }else{
                     width_char = "*";
                 };
